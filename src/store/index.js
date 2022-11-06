b1e6ed0651803591132c285344d4d94df3ef17f4
momo <peanutsm13@gmail.com>
2022-11-06 22:26:14 +0900

    20221107

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,46 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem('trello-lists')
+
+const store = new Vuex.Store({
   state: {
+    lists:savedLists ? JSON.parse(SVGAElement):[
+      {
+        title:'Backlog',
+        cards:[
+          {body:'English'},
+          {body:'Mathmatics'},
+        ]
+      },
+      {
+        title:'Todo',
+        cards:[
+          {body:'Science'}
+        ]
+      },{
+        title:'Doing',
+        cards:[]
+      }
+    ],
   },
   getters: {
   },
   mutations: {
+    addlist(state,payload){
+      state.lists.push({title:payload.title,cards:[]})
+    },
   },
   actions: {
-  },
-  modules: {
+    addlist(context,payload){
+      context.commit('addlist',payload)
+      console.log(payload)
+    },
   }
 })
+
+store.subscribe((mutation,state)=>{
+  localStorage.setItem('trello-lists',JSON.stringify(state.lists))
+})
+
+export default store
