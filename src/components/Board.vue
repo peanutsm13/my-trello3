43de99d3d61947e3aed220fecc4d7cb4c9b355fb
momo <peanuts_m13@outlook.com>
2022-11-09 16:17:41 +0900

    20221110

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,16 +3,35 @@
         <header>my Trello</header>
         <main>
             <p class="info-line">All: 0 tasks</p>
-            <list-add />
+            <div class="list-index">
+                <!-- リストから値を展開し現在の値：itemとそのindexを受け取ることができる -->
+                <!-- itemから更にリストで定義されたオブジェクトのキーで値にアクセスできる -->
+                <!-- v-bind(:)することで、バインドさせたコンポーネント(List.vue)に値を渡すことができる -->
+                <list
+                    v-for="(item, index) in lists"
+                    :key="item.id"
+                    :title="item.title"
+                    :listindex="index"
+                />
+                <list-add />
+            </div>
         </main>
     </div>
 </template>
 
 <script>
 import ListAdd from "./ListAdd.vue";
+import List from "./List.vue";
+import { mapState } from "vuex";
 export default {
     components: {
         ListAdd,
+        List,
+    },
+    // index.jsのstateのリストデータlistsをコンポーネントのcomputedプロパティで取得
+    computed: {
+        //mapState:stateで定義されたデータの名前と同じ名前の文字列でstateを呼び出すことができる
+        ...mapState(["lists"]),
     },
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <div class="listheader">
+            <p class="listtitle">
+                {{ title }}
+            </p>
+            <div class="deletelist" @click="removelist">X</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    //親コンポーネント(ListAdd?)から受け取るデータを定義
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        listindex: {
+            type: Number,
+            required: true,
+        },
+    },
+    methods: {
+        removelist: function () {
+            if (confirm("本当にこのリストを削除しますか？")) {
+                this.$store.dispatch("removelist", {
+                    listindex: this.listindex,
+                });
+            }
+        },
+    },
+};
+</script>
